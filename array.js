e2d0e2d8fb15002841c2af271b619300c0b436cb
Snel <snelson10@sfccmo.edu>
2018-01-20 06:59:01 +0000

    Done.

--- array.js
+++ array.js
@@ -7,11 +7,12 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
@@ -19,10 +20,13 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+    array.unshift(element);
+    return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
+  array.slice(0);
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
